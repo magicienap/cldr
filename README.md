f33777cebdb9841fdb6b8864a4cb715a03b12842
Antoine Proulx <antoine@antoine-proulx.ca>
2014-07-01 13:43:45 -0400

    Add additional information about the project.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Cldr
-====
+# cldr
+> cldr is a library to use information from [CLDR](http://cldr.unicode.org/) data.
 
-** TODO: Add description **
+## Plural Rules
+
+To extract the plural rules from CLDR data, you need the file `common/supplemental/plurals.xml` from the core archive. Then, use `Cldr.PluralRules.Extract.from("path/to/plurals.xml")`.
--- mix.exs
+++ mix.exs
@@ -5,26 +5,32 @@ defmodule Cldr.Mixfile do
     [app: :cldr,
      version: "0.0.1",
      elixir: "~> 0.14.2",
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    cldr is a library to use information from CLDR data.
+    """
+  end
+
+  defp package do
+    [
+      contributors: ["Antoine Proulx"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/magicienap/cldr"
+      }
+    ]
   end
 end
